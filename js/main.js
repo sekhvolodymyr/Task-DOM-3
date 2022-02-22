4228cddcc99643bf795de8d32d2d2b1e82c43fd8
Volodymyr <volodymyr.sekh.dev@gmail.com>
2022-02-22 09:12:33 +0200

    add function in js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let d = document.getElementById("myDiv");
+document.getElementById("myBtn").addEventListener("mouseover", function() {
+    d.innerHTML = `<p class="onme">Mouse on me!</p><br>`;  
+});
+
+document.getElementById("myBtn").addEventListener("click", function() {
+  d.innerHTML = `<p class="click">I was pressed!</p><br>`;  
+});
+
+document.getElementById("myBtn").addEventListener("mouseout", function() {
+  d.innerHTML = `<p class="click">Mouse is not on me!</p><br>`;  
+});
