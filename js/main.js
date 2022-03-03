0f8c1a6ae55437f7d25ce845949c56715341e992
Volodymyr Sekh <89654865+sekhvolodymyr@users.noreply.github.com>
2022-03-03 14:55:43 +0200

    Fix bugs with space

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let d = document.getElementById("myDiv");
 document.getElementById("myBtn").addEventListener("mouseover", function() {
-    d.innerHTML += `<p class="onme">Mouse on me!</p><br>`;  
+    d.innerHTML += `<p class="onme">Mouse on me!</p>`;  
 });
 
 document.getElementById("myBtn").addEventListener("click", function() {
-  d.innerHTML += `<p class="click">I was pressed!</p><br>`;  
+  d.innerHTML += `<p class="click">I was pressed!</p>`;  
 });
 
 document.getElementById("myBtn").addEventListener("mouseout", function() {
-  d.innerHTML += `<p class="click">Mouse is not on me!</p><br>`;  
+  d.innerHTML += `<p class="click">Mouse is not on me!</p>`;  
 });
